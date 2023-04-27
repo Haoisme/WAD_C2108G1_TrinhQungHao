a6341b4164df0dc1ffde8d69fb0c22b78a9d59c9
student <student@LAB325>
2023-04-27 07:16:04 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WAD_C2108G1_TrinhQungHao.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
